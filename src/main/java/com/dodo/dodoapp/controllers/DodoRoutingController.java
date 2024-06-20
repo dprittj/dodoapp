72756e6d3a5e621ac6fb0c7d0bdfbaa970da6a56
dprittj <dprittj@gmail.com>
2024-06-19 21:37:32 -0500

    once again repackaged formerly dodorouting into usercontroller, and refactored templates stru ture to remove dodo package and slide login into the users folder

--- src/main/java/com/dodo/dodoapp/controllers/DodoRoutingController.java
+++ src/main/java/com/dodo/dodoapp/controllers/DodoRoutingController.java
@@ -7,45 +7,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-@RequestMapping("dodo")
+//@Controller
+//@RequestMapping("dodo")
 public class DodoRoutingController {
 
-    //  href (button, hyperlink) resource return
-
-        // login page/form
-    @GetMapping("login")
-    public String renderLogin(){
-        return "/dodo/login";
-    }
-
-        // user page (requires login or user creation)
-    @GetMapping("/users/user")
-    public String renderUserPage(){
-        return "/dodo/users/user";
-    }
-
-
-    //  form resource return
-
-        // from existing-user login to user page
-    @PostMapping("/users/user")
-    public String userLogin(@RequestParam String username, String password) {
-        return "/dodo/users/user";
-    }
-
-        // from new-user button to create-user page
-    @PostMapping("users/new")
-    public String renderCreateNewUser(@RequestParam String username, String password){
-        return "/dodo/users/new";
-    }
-
-        // from create-user page to user page
-    @PostMapping("users/create")
-    public String renderCreate(@RequestParam String firstname, String lastname, String location, String email){
-        return "/dodo/users/user";
-    }
-
+//    //  href (button, hyperlink) resource return
+//
+//        // login page/form
+//    @GetMapping("login")
+//    public String renderLogin(){
+//        return "login";
+//    }
+//
+//        // user page (requires login or user creation)
+//    @GetMapping("/users/user")
+//    public String renderUserPage(){
+//        return "/dodo/users/user";
+//    }
+//
+//
+//    //  form resource return
+//
+//        // from existing-user login to user page
+//    @PostMapping("/users/user")
+//    public String userLogin(@RequestParam String username, String password) {
+//        return "/dodo/users/user";
+//    }
+//
+//        // from new-user button to create-user page
+//    @PostMapping("users/new")
+//    public String renderCreateNewUser(@RequestParam String username, String password){
+//        return "/dodo/users/new";
+//    }
+//
+//        // from create-user page to user page
+//    @PostMapping("users/create")
+//    public String renderCreate(@RequestParam String firstname, String lastname, String location, String email){
+//        return "/dodo/users/user";
+//    }
+//
 
 
 }
--- src/main/java/com/dodo/dodoapp/controllers/UserController.java
+++ src/main/java/com/dodo/dodoapp/controllers/UserController.java
@@ -1,8 +1,55 @@
 package com.dodo.dodoapp.controllers;
 
+import com.dodo.dodoapp.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("users")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //  href (button, hyperlink) resource return
+        // login page/form
+    @GetMapping("/login")
+    public String renderLogin(){
+        return "users/login";
+    }
+
+
+    // user page (requires login or user creation)
+    @GetMapping("/user")
+    public String renderUserPage(){
+        return "/users/user";
+    }
+
+
+    //  form resource return
+        // from existing-user login to user page
+    @PostMapping("/user")
+    public String userLogin(@RequestParam String username, String password) {
+        return "/users/user";
+    }
+
+    // from new-user button to create-user page
+    @PostMapping("/new")
+    public String renderCreateNewUser(@RequestParam String username, String password){
+        return "/users/new";
+    }
+
+    // from create-user page to user page
+    @PostMapping("/create")
+    public String renderCreate(@RequestParam String firstname, String lastname, String location, String email){
+        return "/users/user";
+    }
+
+
+
 
 }
